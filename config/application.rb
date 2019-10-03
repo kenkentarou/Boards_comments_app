724ac941e378399d6169a28af3ccaf1b3c3d7d77
Taro <zettaigoukaku547@gmail.com>
2019-10-03 22:33:31 +0900

    [add]日本語化対応設定

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,7 @@ module RunteqNormal
 
     # Rails(Activerecord)がDBへのRead・Writeを行う際タイムゾーン
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
