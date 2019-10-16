7e1551477e9ca678195fc05180156ba2dda45d20
Taro <zettaigoukaku547@gmail.com>
2019-10-16 23:37:24 +0900

    [modify]migration,seedsファイル修正

--- db/migrate/20191010054110_create_boards.rb
+++ db/migrate/20191010054110_create_boards.rb
@@ -1,11 +1,12 @@
 class CreateBoards < ActiveRecord::Migration[5.2]
   def change
     create_table :boards do |t|
-      t.string :title
-      t.text :body
-      t.integer :user_id
+      t.string :title, null: false
+      t.text :body, null: false
+      t.references :user
 
       t.timestamps
     end
+    add_foreign_key :boards, :users
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-11.times do
-  Board.create(
-      title: Faker::Book.title,
-      body: Faker::JapaneseMedia::OnePiece.akuma_no_mi,
-      user_id: Faker::Number.digit
+10.times do |n|
+  # usersテーブルのダミーデータ生成
+  id = n+1
+  email = Faker::Internet.email
+  last_name = Faker::Name.last_name
+  first_name = Faker::Name.first_name
+  password = "pass"
+
+  User.create!(id: id,
+               email: email,
+               last_name: last_name,
+               first_name: first_name,
+               password: password,
+               password_confirmation: password
+  )
+
+  # boardsテーブルのダミーデータ生成
+  title = Faker::Book.title
+  body = Faker::JapaneseMedia::OnePiece.akuma_no_mi
+  user_id = User.find((n+1).to_s)
+
+  Board.create!(id: id,
+                title: title,
+                body: body,
+                user: user_id
   )
 end
