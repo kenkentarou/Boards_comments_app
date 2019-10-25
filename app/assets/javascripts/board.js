b8b1777a8a546746411117cafafc1bca42c37027
Taro <zettaigoukaku547@gmail.com>
2019-10-25 21:06:55 +0900

    [modify]require,id名修正

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -3,13 +3,13 @@ $(function () {
         if (input.files && input.files[0]) {
             const reader = new FileReader();
             reader.onload = function (e) {
-                $('#js_img_prev').attr('src', e.target.result);
+                $('#js-img-prev').attr('src', e.target.result);
             }
             reader.readAsDataURL(input.files[0]);
         }
     }
 
-    $("#js_board_img").change(function () {
+    $('#js-board-img').change(function () {
         readURL(this);
     });
 })
