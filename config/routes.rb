9c9e870c688085fb784fb25a04bea266471ccd17
Taro <zettaigoukaku547@gmail.com>
2019-11-01 00:01:07 +0900

    [add]ルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :boards, only: %i[index new create]
+  get 'comments/index'
+  resources :boards, only: %i[index new create show]
+  resources :boards do
+    resources :comments, only: %i[create destroy], shallow: true
+  end
   root 'static_pages#top'
   resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
