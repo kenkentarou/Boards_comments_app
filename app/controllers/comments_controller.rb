c2a43a79413046c76879b4f70c69c70d9a51cb01
Taro <zettaigoukaku547@gmail.com>
2019-11-02 08:51:52 +0900

    [modify]sider指摘修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,9 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy;
+  end
+
   private
 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'comments/index'
   resources :boards, only: %i[index new create show]
   resources :boards do
-    resources :comments, except: %i[:edit, :update, :destroy], shallow: true
+    resources :comments, only: %i[create destroy], shallow: true
   end
   root 'static_pages#top'
   resources :users, only: %i[new create]
