c2f305c5ca5cf69f3b52514bc5bf5cc11be7bbc0
Taro <zettaigoukaku547@gmail.com>
2019-11-02 16:33:08 +0900

    [modify]ネスト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'comments/index'
-  resources :boards, only: %i[index new create show]
-  resources :boards do
+  resources :boards, only: %i[index new create show] do
     resources :comments, only: %i[create destroy], shallow: true
   end
   root 'static_pages#top'
