a265c3b1b8c65f7edc979dbc842ff447d870a506
Taro <zettaigoukaku547@gmail.com>
2019-11-19 16:22:24 +0900

    sider指摘修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,7 +3,7 @@ class BookmarksController < ApplicationController
     board = Board.find(params[:board_id])
     current_user.like(board)
     respond_to do |format|
-      format.js { render template: 'bookmarks/create', locals: {board: board} }
+      format.js { render template: 'bookmarks/create', locals: { board: board } }
     end
   end
 
@@ -11,7 +11,7 @@ class BookmarksController < ApplicationController
     board = Board.find(params[:id])
     current_user.unlike(board)
     respond_to do |format|
-      format.js { render template: 'bookmarks/destroy', locals: {board: board} }
+      format.js { render template: 'bookmarks/destroy', locals: { board: board } }
     end
   end
 end
