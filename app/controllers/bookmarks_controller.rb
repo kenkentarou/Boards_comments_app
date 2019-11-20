6d27ddbca75cbb4b56119a8dc083ab403afda53e
Taro <zettaigoukaku547@gmail.com>
2019-11-20 20:49:27 +0900

    [modify]respond削除

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,11 @@
 class BookmarksController < ApplicationController
   def create
-    board = Board.find(params[:board_id])
-    current_user.like(board)
-    respond_to do |format|
-      format.js { render template: 'bookmarks/create', locals: { board: board } }
-    end
+    @board = Board.find(params[:board_id])
+    current_user.like(@board)
   end
 
   def destroy
-    board = Board.find(params[:id])
-    current_user.unlike(board)
-    respond_to do |format|
-      format.js { render template: 'bookmarks/destroy', locals: { board: board } }
-    end
+    @board = Board.find(params[:id])
+    current_user.unlike(@board)
   end
 end
