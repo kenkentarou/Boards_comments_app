37ba021822ebc074188ef7390e636253de2edd33
Taro <zettaigoukaku547@gmail.com>
2019-11-26 16:12:13 +0900

    [modify]改行調整

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -5,12 +5,14 @@ $(function () {
     $('#js-comment-' + id).hide();
     $('#js-textarea-comment-box-' + id).show();
   });
+
   $(document).on('click', '.js-button-edit-comment-cancel', function () {
     // 非表示
     const id = $(this).data('comment-id');
     $('#js-comment-' + id).show();
     $('#js-textarea-comment-box-' + id).hide();
   });
+
   $(document).on('click', '.js-button-comment-update', function () {
     const id = $(this).data('comment-id');
     const textField = $('#js-textarea-comment-' + id);
