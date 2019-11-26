02cfb14e401f8b8943ddf9d50da352a1a456f3e3
Taro <zettaigoukaku547@gmail.com>
2019-11-26 13:48:57 +0900

    [modify]var→const修正等

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,24 +1,18 @@
 $(function () {
     $(document).on('click', '.js-edit-comment-button', function () {
         // 表示
-        var id = $(this).data('id');
+        const id = $(this).data('id');
         $('#js-comment-' + id).hide();
         $('#js-textarea-comment-box-' + id).show();
     });
-})
-
-$(function () {
     $(document).on('click', '.js-button-edit-comment-cancel', function () {
         // 非表示
-        var id = $(this).data('comment-id');
+        const id = $(this).data('comment-id');
         $('#js-comment-' + id).show();
         $('#js-textarea-comment-box-' + id).hide();
     });
-})
-
-$(function () {
     $(document).on('click', '.js-button-comment-update', function () {
-        var id = $(this).data('comment-id');
+        const id = $(this).data('comment-id');
         const textField = $('#js-textarea-comment-' + id);
         const body = textField.val();
         $.ajax({
