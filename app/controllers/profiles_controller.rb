b4ee83623eb508c1924e5cf99b48f2cf13816773
Taro <zettaigoukaku547@gmail.com>
2019-12-02 11:10:03 +0900

    [add]controller,routes

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,22 @@
+class ProfilesController < ApplicationController
+  def show;
+  end
+
+  def edit;
+  end
+
+  def update
+    if current_user.update(user_update_params)
+      redirect_to(profile_path, success: 'プロフィールを更新しました')
+    else
+      flash.now[:danger] = 'プロフィールの更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def user_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :user_image)
+  end
+end
