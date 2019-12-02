d3fafa5c29f87cbdd55b93a8790e0e62e69b75ec
Taro <zettaigoukaku547@gmail.com>
2019-12-02 11:04:38 +0900

    [add]carrierwaveアップローダー、usersテーブルカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_015150) do
+ActiveRecord::Schema.define(version: 2019_11_30_060902) do
 
   create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_015150) do
     t.string "first_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
