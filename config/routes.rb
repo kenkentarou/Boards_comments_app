8ec969cccb702ca4ab2ac8c4032dcf43396575a5
Taro <zettaigoukaku547@gmail.com>
2019-12-10 21:28:02 +0900

    [add]ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
   resources :bookmarks, only: %i[create destroy]
   resource :profile, only: %i[show edit update]
   resources :password_resets, only: %i[new create edit update]
-  get 'admin', to: 'admin/dashboards#index'
-  get 'admin/login', to: 'admin/user_sessions#new'
-  post 'admin/login', to: 'admin/user_sessions#create'
-  post 'admin/logout', to: 'admin/user_sessions#destroy'
+  namespace :admin do
+    get '/', to: 'dashboards#index'
+    get '/login', to: 'user_sessions#new'
+    post '/login', to: 'user_sessions#create'
+    post '/logout', to: 'user_sessions#destroy'
+  end
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
