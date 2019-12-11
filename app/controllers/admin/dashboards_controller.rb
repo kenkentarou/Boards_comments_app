ccad296bf9509ea7eca05a921d5bb3805cd301b6
Taro <zettaigoukaku547@gmail.com>
2019-12-11 17:25:07 +0900

    [add]一旦push

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardsController < Admin::AdminController
   layout 'admin/layouts/admin'
-  #before_action :authenticate_user
+  before_action :authenticate_user
   #before_action :require_login
   def index;
   end
