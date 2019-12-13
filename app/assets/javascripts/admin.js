07e2cd4c5906990d8b87b6dbf2989ca65ff7e1bc
Taro <zettaigoukaku547@gmail.com>
2019-12-14 01:12:37 +0900

    [add]マニフェストファイルへjsファイル読み込み

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,3 +2,4 @@
 //= require admin-lte/plugins/jquery/jquery.min.js
 //= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js
 //= require admin-lte/dist/js/adminlte.min.js
+//= require common.js
