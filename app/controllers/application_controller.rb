f3e05b24bca9dcf8ef9f234f48fad475e1bdb571
Taro <zettaigoukaku547@gmail.com>
2019-12-17 22:18:04 +0900

    [add]コントローラー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  unless Rails.env.development?
+    rescue_from Exception, with: :render_500
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+  end
+
+  def render_500(e)
+    ExceptionNotifier.notify_exception(e, env: request.env, data: {message: "your error message"})
+  end
+
+  def render_404
+    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+  end
+
 
   private
 
