cfec17ea3c3d976357b81954db42750de9c82906
Taro <zettaigoukaku547@gmail.com>
2019-12-23 16:14:21 +0900

    [add]fontawesome-rails削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,18 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_boards, through: :bookmarks, source: :board
+  has_many :authentications, dependent: :destroy
+  accepts_nested_attributes_for :authentications
   authenticates_with_sorcery!
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: {minimum: 3}, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :email, presence: true, uniqueness: { scope: :reset_password_token }
+  validates :email, presence: true, uniqueness: {scope: :reset_password_token}
 
-  enum role: { general: 0,
-               admin: 1 }
+  enum role: {general: 0,
+              admin: 1}
 
   def own_board?(board)
     id == board.user_id
