9395126a9e83589b77f2d4b79b8f31e833355eb7
Taro <zettaigoukaku547@gmail.com>
2020-03-05 13:27:37 +0900

    seed投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,20 @@
-10.times do |n|
   # usersテーブルのダミーデータ生成
-  id = n+1
-  email = Faker::Internet.email
-  last_name = Faker::Name.last_name
-  first_name = Faker::Name.first_name
-  password = "pass"
-
-  User.create!(id: id,
-               email: email,
-               last_name: last_name,
-               first_name: first_name,
-               password: password,
-               password_confirmation: password
-  )
+  puts 'Start inserting seed "users" ...'
+  20.times do
+    user = User.create!(
+        email: Faker::Internet.unique.email,
+        last_name: Faker::Name.last_name,
+        first_name: Faker::Name.first_name,
+        password: 'password',
+        password_confirmation: 'password',
+        remote_user_image_url: 'https://picsum.photos/350/350/?random'
+    )
+    puts "\"#{user.decorate.full_name}\" has created!"
+  end
 
   # boardsテーブルのダミーデータ生成
-  title = Faker::Book.title
-  body = Faker::JapaneseMedia::OnePiece.akuma_no_mi
-  user_id = User.find((n+1).to_s)
-
-  Board.create!(id: id,
-                title: title,
-                body: body,
-                user: user_id
-  )
-end
+  puts 'Start inserting seed "boards" ...'
+  User.limit(10).each do |user|
+    board = user.boards.create!(title: Faker::Book.title, body: Faker::Hacker.say_something_smart, remote_board_image_url: 'https://picsum.photos/350/350/?random')
+    puts "board#{board.id} has created!"
+  end
